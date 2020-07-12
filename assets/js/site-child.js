fdc375861b564f4971f9bc5830d3f6648fe20ffe
Fabian Kemper <mail@fabiankemper.de>
2020-07-12 12:09:11 +0200

    Initial commit

---
+++ assets/js/site-child.js
@@ -0,0 +1,7 @@
+(function($) {
+
+	// Add your custom scripts here.
+	// This file will be compiled and is part of the gulp workflow.
+	// If you're not using gulp, please make your changes directly to the site-child.js file in the main js folder.
+
+})( jQuery );
---
+++ functions.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Functions.
+ *
+ * @package Page Builder Framework Child
+ */
+
+defined( "ABSPATH" ) || die( "Can't access directly" );
+
+/**
+ * Child theme setup.
+ */
+function wpbf_child_theme_setup() {
+
+	// Textdomain.
+	load_child_theme_textdomain( 'page-builder-framework-child', WPBF_CHILD_THEME_DIR . '/languages' );
+
+}
+add_action( 'after_setup_theme', 'wpbf_child_theme_setup' );
+
+/**
+ * Enqueue scripts & styles.
+ */
+function wpbf_child_scripts() {
+
+	// Styles.
+	wp_enqueue_style( 'wpbf-style-child', WPBF_CHILD_THEME_URI . '/style.css', false, WPBF_CHILD_VERSION );
+
+	// Scripts (uncomment if needed).
+	// wp_enqueue_script( 'wpbf-site-child', WPBF_CHILD_THEME_URI . '/js/site-child.js', false, WPBF_CHILD_VERSION, true );
+
+}
+add_action( 'wp_enqueue_scripts', 'wpbf_child_scripts', 13 );
